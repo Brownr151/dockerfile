dc1986518664825596f2283d06ab6fb21f316af5
Brownr151 <robert_291@msn.com>
2019-04-25 11:23:38 +0100

    12345

--- abc.sh
+++ abc.sh
@@ -4,4 +4,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt update
 sudo apt install -y docker-ce
-docker build https://github.com/Brownr151/dockerfile 
+git clone https://github.com/Brownr151/dockerfile 
+cd dockerfile/
+sudo docker build -t abc .
+sudo docker run -d -it -p 8080:80 abc
